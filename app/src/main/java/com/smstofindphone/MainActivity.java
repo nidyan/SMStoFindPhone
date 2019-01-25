41445ef4779b1ed585fc0932e1d3c44fcf8aaf75
Aryan Raj <amznarya@amazon.com>
2019-01-26 03:57:58 +0530

    Fire new intent on clicking button

--- app/src/main/java/com/smstofindphone/MainActivity.java
+++ app/src/main/java/com/smstofindphone/MainActivity.java
@@ -1,13 +1,38 @@
 package com.smstofindphone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.logging.Logger;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.smstofindphone.message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private static final Logger log = Logger.getLogger(MainActivity.class.getSimpleName());
+
+    public void displayTextOnClick(View view){
+        log.info("Entering displayTextOnClick");
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
     }
 }
